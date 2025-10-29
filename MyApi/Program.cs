b5e93dd3aeb923a133f3ed5bdacd02daf0fb6611
Marco Pacini <m.pacini@man-con.com>
2025-10-29 11:58:45 +0100

    primo commit

---
+++ MyApi/Program.cs
@@ -0,0 +1,25 @@
+using MyApi.Middlewares;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.RegisterHttpServices();
+builder.Services.RegisterServices();
+
+var app = builder.Build();
+
+app.UseMiddleware<GlobalExceptionMiddleware>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    //app.UseExceptionHandler();
+}
+
+app.RegisterToDoItemsEndpoints(); 
+
+app.UseHttpsRedirection();
+app.Run();
