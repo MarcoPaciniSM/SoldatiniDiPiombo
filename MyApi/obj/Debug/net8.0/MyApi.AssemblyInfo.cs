5cefb5a305720601db9f58e3056d70bf6b0c875f
Marco Pacini <m.pacini@man-con.com>
2025-10-29 11:59:01 +0100

    Primo push

--- MyApi/obj/Debug/net8.0/MyApi.AssemblyInfo.cs
+++ MyApi/obj/Debug/net8.0/MyApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5e93dd3aeb923a133f3ed5bdacd02daf0fb6611")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
